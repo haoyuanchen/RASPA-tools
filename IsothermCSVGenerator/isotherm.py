541ac165d2062a40bae0dd3b5740030b48437990
Haoyuan Chen <haoyuan.chen@utrgv.edu>
2022-04-01 19:38:21 -0500

    Update isotherm.py

--- IsothermCSVGenerator/isotherm.py
+++ IsothermCSVGenerator/isotherm.py
@@ -8,15 +8,15 @@ Usage: ./isotherm.py unit
 
 import os,sys
 
-u = sys.argv[1]
+uni = sys.argv[1]
 
-if u == 'mg_per_g':
+if uni == 'mg_per_g':
     cmd = "grep 'Average loading absolute \[milligram\/gram framework\]' Output/System_0/* > uptake_raw"
-elif u == 'mmol_per_g':
+elif uni == 'mmol_per_g':
     cmd = "grep 'Average loading absolute \[mol\/kg framework\]' Output/System_0/* > uptake_raw"
-elif u == 'ml_per_g':
+elif uni == 'ml_per_g':
     cmd = "grep 'Average loading absolute \[cm^3 (STP)\/gr framework\]' Output/System_0/* > uptake_raw"
-elif u == 'ml_per_ml':
+elif uni == 'ml_per_ml':
     cmd = "grep 'Average loading absolute \[cm^3 (STP)\/cm^3 framework\]' Output/System_0/* > uptake_raw"
 os.system(cmd)
 
@@ -41,13 +41,13 @@ ps = sorted(ps)
 us = sorted(us)
 
 f = open("isotherm.csv", 'w')
-if u == 'mg_per_g':
+if uni == 'mg_per_g':
     f.write("Pressure(Pa),Uptake(mg/g)\n")
-elif u == 'mmol_per_g':
+elif uni == 'mmol_per_g':
     f.write("Pressure(Pa),Uptake(mmol/g)\n")
-elif u == 'ml_per_g':
+elif uni == 'ml_per_g':
     f.write("Pressure(Pa),Uptake(ml/g)\n")
-elif u == 'ml_per_ml':
+elif uni == 'ml_per_ml':
     f.write("Pressure(Pa),Uptake(ml/ml)\n")
 for i in range(len(ps)):
     f.write("%f,%f\n"%(ps[i],us[i]))
